a5de3e7f9de76385087e7935b4e15005a4f47c37
Julie ARCHER <julie.archer@viacesi.fr>
2021-07-07 10:54:13 +0200

    Login screen

--- screen/Login.js
+++ screen/Login.js
@@ -1,12 +1,13 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import React, { useContext, useState } from 'react'
-import { StyleSheet, TextInput, Button, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 import { UserContext } from '../App';
 import { userStorageKey } from '../service/userService';
 
 export default function Login({navigation}) {
 
+	const [message, setMessage] = useState('');
 	const [email, setEmail] = useState('');
 	const [pwd, setPwd] = useState('');
 	const key = 'authToken';
@@ -15,39 +16,28 @@ export default function Login({navigation}) {
 	console.log('login');
 	const handlePress = () => {
 		const data = {
-			identifier: email,
+			email: email,
 			password: pwd
 		};
-		//const url = 'http://localhost:1337/auth/local';
 		const url = 'http://localhost/projetCubes3/Models/user/selectUser.php'
 		axios.post(url, data)
 		.then((response)=> {
-			console.log(response);
-			const {jwt, user} = response.data;
-			AsyncStorage.setItem(key, jwt);
-			AsyncStorage.setItem(userStorageKey, JSON.stringify(user));
-			axios.defaults.headers['Authorization'] = 'Bearer ' + jwt;
-			context.setUser(user);
-			navigation.replace('home');
-		})
-		.catch(function (error) {
-			if (error.response) {
-			  // The request was made and the server responded with a status code
-			  // that falls out of the range of 2xx
-			  console.log(error.response.data);
-			  console.log(error.response.status);
-			  console.log(error.response.headers);
-			} else if (error.request) {
-			  // The request was made but no response was received
-			  // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-			  // http.ClientRequest in node.js
-			  console.log(error.request);
-			} else {
-			  // Something happened in setting up the request that triggered an Error
-			  console.log('Error', error.message);
+			const result = response.data;
+			if (result.state == "success") {	
+				console.log("ok");
+				AsyncStorage.setItem(userStorageKey, JSON.stringify(result.data));
+				context.setUser(result.data);
+				navigation.replace('home');
 			}
-			console.log(error.config);
-		  });;
+			else {
+				console.log("pas ok");
+				setMessage(result.message);
+			}
+		})
+	}
+
+	const registerPress = () => {
+		console.log("go register !! ");
 	}
 
 	return (
@@ -64,9 +54,18 @@ export default function Login({navigation}) {
 				placeholder="Mot de passe"
 				style = {[styles.base, styles.border]}
 			/>
+			<Text
+				style = {styles.error}
+			>{message}
+			</Text>
 			<Button
 				title = "Connexion"
 				onPress = {handlePress}
+				style = {styles.base}
+			/>
+			<Button
+				title = "Créer un compte"
+				onPress = {registerPress}
 				style = {styles.base}
 			/>
 		</View>
@@ -81,5 +80,9 @@ const styles = StyleSheet.create({
 	border: {
 		borderColor: 'black',
 		borderWidth: 1
+	},
+	error: {
+		color: 'red',
+		textAlign: 'center'
 	}
 });
