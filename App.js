087d0d3b470b89c5d35a03bcfff317de1dc91e89
user.nale <loic.albert.denis@gmail.com>
2021-07-07 16:08:02 +0200

    app

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState, useContext} from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,46 +8,92 @@ import { getUser } from './service/userService';
 import Home from './screen/Home';
 import Moncompte from './screen/Moncompte';
 import Register from './screen/Register';
+import Admin from './screen/Admin';
 
 const Stack = createStackNavigator();
 
 export const UserContext = createContext({
-  user : {},
-  setUser: () => {}
+  user: {},
+  setUser: () => { }
 });
 
 export default function App() {
 
-	const [user, setUser] = useState('');
+  const [user, setUser] = useState('');
 
-	useEffect(() => {
-		getUser().then(result => setUser(result));
-	}, []);
+  useEffect(() => {
+    getUser().then(result => setUser(result));
+  }, []);
   const Tab = createBottomTabNavigator();
+  const userContextValue = {
+    user: user,
+    setUser: setUser
+  };
+  const SettingsStack = createStackNavigator();
 
-	const userContextValue = {
-		user: user,
-		setUser: setUser
-	};
+  function HomeStackScreen() {
+    return (
+      <SettingsStack.Navigator>
+        <SettingsStack.Screen name="home" component={Home} />
+        <SettingsStack.Screen name="login" component={Login} />
+        <SettingsStack.Screen name="register" component={Register} />
+      </SettingsStack.Navigator>
+    );
+  }
+  function AccountStackScreen() {
+   
+    return (
+      <SettingsStack.Navigator>
+        <SettingsStack.Screen name="Mon compte" component={Moncompte} />
+      </SettingsStack.Navigator>
+    );
+  }
+  function LoginStackScreen() {
+    return (
+      <SettingsStack.Navigator>
+        <SettingsStack.Screen name="login" component={Login}/>
+        <SettingsStack.Screen name="register" component={Register}/>
+      </SettingsStack.Navigator>
+    );
+  }
+  console.log(user.email);
+  if(user.email !== undefined){
+    if(user.id_role === 0){
+      return (
+        <UserContext.Provider value={userContextValue}>
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name="home" component={HomeStackScreen}/>
+              <Tab.Screen name="account" component={AccountStackScreen}/>
+              <Tab.Screen name="admin" component={Admin}/>
+            </Tab.Navigator>
+          </NavigationContainer>
+        </UserContext.Provider>
+      );
+    }
+    return (
+      <UserContext.Provider value={userContextValue}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="home" component={HomeStackScreen}/>
+            <Tab.Screen name="account" component={AccountStackScreen}/>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </UserContext.Provider>
+    );
+  }else{
+    return (
+      <UserContext.Provider value={userContextValue}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="home" component={HomeStackScreen}/>
+            <Tab.Screen name="login" component={LoginStackScreen}/>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </UserContext.Provider>
+    );
+  }
 
-  return (
-	<UserContext.Provider value = {userContextValue}>
-		{/* <NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="login" component={Login} />
-				<Stack.Screen name="home" component={Home} />
-				<Stack.Screen name="Mon Compte" component={Moncompte} />
-				<Stack.Screen name="register" component={Register} />
-			</Stack.Navigator>
-		</NavigationContainer> */}
-    <NavigationContainer>
-      <Tab.Navigator>
-      <Tab.Screen name="login" component={Login}  />
-      <Tab.Screen name="register" component={Register}  />
-      </Tab.Navigator>
-    </NavigationContainer>
-	</UserContext.Provider>
-  );
 }
 
 const styles = StyleSheet.create({});
